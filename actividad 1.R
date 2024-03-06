7fcf034b4db192e7284bff6f1fed5ba44383184a
luccafrachelle <134110236+luccafrachelle@users.noreply.github.com>
2024-03-06 12:02:44 -0300

    Add files via upload

---
+++ actividad 1.R
@@ -0,0 +1,30 @@
+1) double , char , char , int , double
+
+x = c(3, 2, 2, 4, 5)
+y = c(1, 6, 5, 5, 9)
+
+z = 2*(x[1]) + 3*(x[4]) + 5*(sqrt(y[2]))
+sum(y)
+sum((y)^2)
+(sum(y)^2)
+sum(((y)^x))
+mean(x)
+mean(y)
+median(x)
+median(y)
+sum(x*y)
+lista = list(c(1:4))
+lista = list("a")
+lista = list(1 , 2 , "a" , "b")
+lista = list(FALSE , FALSE , FALSE)
+lista = list(100)
+lista[3]*8
+lista[1][3]*8
+lista[4][2]
+x <- c("bueno", "muy bueno", "excelente", "bueno")
+x_new = factor(x, levels =c("malo", "bueno", "muy bueno", "excelente") , ordered = TRUE)
+new_xx = ordered()
+
+
+
+
